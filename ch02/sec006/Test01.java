5977511bd1c705340f55ef5563e33e1618ee4ec1
gyurikim-1202 <2december@naver.com>
2023-04-14 16:15:57 +0900

    데이터변환과 데이터범위

---
+++ ch02/sec006/Test01.java
@@ -0,0 +1,17 @@
+package ch02.sec006;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 3;
+		
+		double c = 0;
+		
+		c = (float)a/b;
+		
+		System.out.printf("값은%10.4f", c);
+	}
+
+}
